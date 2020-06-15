c133851b5e0de503de2a06856d2ceab299b78cd7
asuka <asuka.altair@gmail.com>
2020-06-15 21:51:06 +0900

    [add] 5-9 sign-up sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+	before_action :configure_purmitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_purmitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+	end
+
 end
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+end
